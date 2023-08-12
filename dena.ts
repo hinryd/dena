614418a53656c4e61e70523af65df0aaca7ca4da
hinryd <lkhh@protonmail.com>
2023-08-13 04:59:44 +0800

    refactor: change Dena class API

--- dena.ts
+++ dena.ts
@@ -4,29 +4,33 @@
 import Base from "./base.ts";
 import Drive from "./drive.ts";
 
-class Dena {
-  private key: string;
-  private id: string;
-  // private options: Options;
-  constructor(key: string, id?: string) {
-    //TODO: make sure that the way to create keys will not changed
-    if (key.length === 0) throw new Error("project key is empty");
-    if (!id) id = key.split("_")[0];
+export default class Dena {
+  private readonly key: string;
+  private readonly id: string;
+
+  constructor(key: string) {
+    if (key.length === 0) throw new Error("Project key is empty");
     this.key = key;
-    this.id = id;
-    // this.options = defaultize(defaults.options, options)
+    this.id = key.split("_")[0];
   }
 
   /**
+   * Create a new Base instance
+   *
+   * @param name Base name
    * @returns Base instance
    */
-  Base<T>(name: string) {
+  getBase<T>(name: string) {
     return new Base<T>(this.key, this.id, name);
   }
 
-  Drive(name: string) {
+  /**
+   * Create a new Drive instance
+   *
+   * @param name Drive name
+   * @returns Drive instance
+   */
+  getDrive(name: string) {
     return new Drive(this.key, this.id, name);
   }
 }
-
-export { Dena as default };
